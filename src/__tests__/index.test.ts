d55bbbb3943496add934b1e96b378711ad72df97
FoggDev <foggdev@gmail.com>
2023-09-30 22:43:39 -0600

    Initial commit

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,65 @@
+import { jest, afterEach, describe, expect, it } from '@jest/globals'
+
+import fetchMock from 'jest-fetch-mock'
+
+import api from '../index'
+
+// Global fetch mock
+;(global.fetch as any) = fetchMock
+
+describe('api', () => {
+  afterEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  describe('get', () => {
+    it('fetches data successfully', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ data: 'some data' }))
+      const result = await api.get('https://api.example.com/data')
+      expect(result).toEqual({ data: 'some data' })
+    })
+
+    it('throws an error when fetch fails', async () => {
+      fetchMock.mockReject(new Error('fake error message'))
+      await expect(api.get('https://api.example.com/data')).rejects.toThrow('fake error message')
+    })
+  })
+
+  describe('post', () => {
+    it('posts data successfully', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({ success: true }))
+      const result = await api.post('https://api.example.com/data', { name: 'test' })
+      expect(result).toEqual({ success: true })
+    })
+  })
+
+  describe('handleError', () => {
+    it('handles a 400 Bad Request', async () => {
+      const mockResponse = new Response(JSON.stringify({ message: 'Bad Request' }), {
+        status: 400
+      })
+      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+      await api.handleError(mockResponse)
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Bad request:', { message: 'Bad Request' })
+      consoleErrorSpy.mockRestore()
+    })
+  })
+
+  describe('handleResponse', () => {
+    it('handles successful response', () => {
+      const response = api.handleResponse({ data: 'some data', cache: true, status: 200 })
+      expect(response).toEqual({
+        system: { cache: true, error: false, status: 200 },
+        response: { data: 'some data' }
+      })
+    })
+
+    it('handles error response', () => {
+      const response = api.handleResponse({ error: 'some error', cache: false, status: 500 })
+      expect(response).toEqual({
+        system: { cache: false, error: true, status: 500 },
+        response: { error: 'some error' }
+      })
+    })
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,98 @@
+type RequestHeaders = { [key: string]: string }
+type RequestBody = { [key: string]: any }
+type Args = {
+  data?: any
+  error?: any
+  cache?: boolean
+  status?: number
+}
+
+const api = {
+  async get(url: string, headers?: RequestHeaders) {
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: headers
+      })
+
+      return response.json()
+    } catch (error) {
+      console.error('Failed to fetch data:', error)
+      throw error
+    }
+  },
+  async post(url: string, data: RequestBody, headers?: RequestHeaders) {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', ...headers },
+        body: JSON.stringify(data)
+      })
+
+      return response.json()
+    } catch (error) {
+      console.error('Failed to post data:', error)
+      throw error
+    }
+  },
+  async put(url: string, data: RequestBody, headers?: RequestHeaders) {
+    try {
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', ...headers },
+        body: JSON.stringify(data)
+      })
+
+      return response.json()
+    } catch (error) {
+      console.error('Failed to update data:', error)
+      throw error
+    }
+  },
+  async delete(url: string, headers?: RequestHeaders) {
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+        headers: headers
+      })
+
+      return response.json()
+    } catch (error) {
+      console.error('Failed to delete data:', error)
+      throw error
+    }
+  },
+  async handleError(response: Response) {
+    const data = await response.json()
+
+    switch (response.status) {
+      case 400:
+        console.error('Bad request:', data)
+        break
+      case 404:
+        console.error('Not found:', data)
+        break
+      default:
+        console.error('API call failed:', data)
+    }
+  },
+  handleResponse({ data, error, cache = false, status = 200 }: Args) {
+    if (error) {
+      return {
+        system: { cache, error: true, status: status || 500 },
+        response: {
+          error
+        }
+      }
+    }
+
+    return {
+      system: { cache, error: false, status: status || 200 },
+      response: {
+        data
+      }
+    }
+  }
+}
+
+export default api
